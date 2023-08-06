268b41e565eb65993df5e932fea62cb80552c47c
Zimu Luo <536682885@qq.com>
2023-08-06 17:47:26 +0800

    Added arrangement by Maven

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module pers.ericmonlye.jsonparser {
-	exports pers.ericmonlye.jsonparser.parser;
-	exports pers.ericmonlye.jsonparser.exception;
-	
-    requires java.base;
-    requires org.junit.jupiter.api;
-}
